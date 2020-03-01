41a08a8740473adba1ae98208689b3d211fda979
Dorukhan Tokay <tokay.dorukhan@hotmail.com>
2020-03-01 19:38:27 +0300

    not needed

--- BaseLib/Networking/DnsUtils.cs
+++ BaseLib/Networking/DnsUtils.cs
@@ -7,17 +7,9 @@ namespace BaseLib.Networking
         [DllImport("dnsapi.dll", EntryPoint = "DnsFlushResolverCache")]
         private static extern uint DnsFlushResolverCache();
 
-        [DllImport("dnsapi.dll", EntryPoint = "DnsFlushResolverCacheEntry_A")]
-        private static extern int DnsFlushResolverCacheEntry(string hostName);
-
         public static void FlushCache()
         {
             DnsFlushResolverCache();
-        }
-
-        public static void FlushCache(string hostName)
-        {
-            DnsFlushResolverCacheEntry(hostName);
         }
     }
 }
